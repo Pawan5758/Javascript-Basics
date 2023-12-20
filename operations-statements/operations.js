5619708ea845101d8adbd35934a49e70eb1d10a3
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-20 17:50:09 +0545

    logical operators

---
+++ operations-statements/operations.js
@@ -0,0 +1,10 @@
+// logical operations
+const a = prompt("Enter number between 1 to 9");
+const b = prompt("Enter number between 1 to 9 ");
+
+if (a > b) {
+  alert(a + " is greater than " + b);
+} else {
+  alert(a + "is smaller than" + b);
+}
+// conversion
