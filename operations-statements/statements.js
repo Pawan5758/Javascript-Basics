b3f5d7e54048ba190b66f310f8a5be26e0eea818
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-20 18:24:36 +0545

    switch case

---
+++ operations-statements/statements.js
@@ -0,0 +1,24 @@
+// switch case
+const value = Number(prompt("enter a value"));
+switch (value) {
+  case 0:
+    alert("Sunday");
+    break;
+  case 1:
+    alert("monday");
+    break;
+  case 2:
+    alert("Tuesday");
+  //     break;
+  //   case 3:
+  //     day = "Wednesday";
+  //     break;
+  //   case 4:
+  //     day = "Thursday";
+  //     break;
+  //   case 5:
+  //     day = "Friday";
+  //     break;
+  //   case 6:
+  //     day = "Saturday";
+}
