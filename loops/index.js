85623b0969562f9aeb77597cae371b025dd687b3
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-21 18:01:34 +0545

    multiplication table using loops

---
+++ loops/index.js
@@ -0,0 +1,24 @@
+// write a loop that prints from 1 to 100
+
+// for (let a = 1; a < 100; a++) {
+//   console.log(a);
+// }
+
+// let a = 1;
+// while (a < 10) {
+//   a++;
+// }
+
+// program to generate a multiplication table
+
+// take input from the user
+const number = Number(prompt("Enter an integer: "));
+
+//creating a multiplication table
+for (let i = 1; i <= 10; i++) {
+  // multiply i with number
+  const result = i * number;
+
+  // display the result
+  console.log(`${number} * ${i} = ${result}`);
+}
