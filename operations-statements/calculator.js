5ad54251e9dd7dc7cffbeb481929cca2204dceaa
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-21 17:39:51 +0545

    ternary operation

--- operations-statements/calculator.js
+++ operations-statements/calculator.js
@@ -1,32 +1,32 @@
-// program for a simple calculator
-let result;
+// // program for a simple calculator
+// let result;
 
-// take the operand input
-const number1 = Number(prompt("Enter first number: "));
-const number2 = Number(prompt("Enter second number: "));
+// // take the operand input
+// const number1 = Number(prompt("Enter first number: "));
+// const number2 = Number(prompt("Enter second number: "));
 
-// take the operator input
-const operator = prompt("Enter operator ( either +, -, * or / ): ");
+// // take the operator input
+// const operator = prompt("Enter operator ( either +, -, * or / ): ");
 
-switch (operator) {
-  case "+":
-    result = number1 + number2;
-    break;
+// switch (operator) {
+//   case "+":
+//     result = number1 + number2;
+//     break;
 
-  case "-":
-    result = number1 - number2;
-    break;
+//   case "-":
+//     result = number1 - number2;
+//     break;
 
-  case "*":
-    result = number1 * number2;
-    break;
+//   case "*":
+//     result = number1 * number2;
+//     break;
 
-  case "/":
-    result = number1 / number2;
-    break;
+//   case "/":
+//     result = number1 / number2;
+//     break;
 
-  default:
-    console.log("Invalid operator");
-    break;
-}
-console.log(result);
+//   default:
+//     console.log("Invalid operator");
+//     break;
+// }
+// console.log(result);
--- operations-statements/ternaryoperator.js
+++ operations-statements/ternaryoperator.js
@@ -1,1 +1,4 @@
 // write ternary operator to check if a user can vote or not (age)
+const age = Number(prompt("enter your age"));
+const person = age > 18 ? "valid" : "invalid";
+console.log(person);
