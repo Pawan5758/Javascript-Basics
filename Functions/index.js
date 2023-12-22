6ee5dae7217ec3d3ae40c2b4d7697a896c2c4d76
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-22 17:22:31 +0545

    adding using function

---
+++ Functions/index.js
@@ -0,0 +1,9 @@
+// write a function to add numbers
+const a = Number(prompt("enter any number"));
+const b = Number(prompt("enter any number"));
+
+function add(a, b) {
+  return a + b;
+}
+const c = add(a, b);
+console.log(prompt("the sum of" + a + "+" + b + "=" + c));
