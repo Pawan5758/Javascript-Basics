9acc8e6e998b15f5abc697788bd6c4885f0fdf58
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-22 17:38:22 +0545

    converting fahrenheit to celsius in function

--- Functions/index.js
+++ Functions/index.js
@@ -1,15 +1,30 @@
 // write a function to add numbers
-const a = Number(prompt("enter any number"));
-const b = Number(prompt("enter any number"));
+// const a = Number(prompt("enter any number"));
+// // const b = Number(prompt("enter any number"));
 
-function add(a, b) {
-  return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
 
-function checkEven(a) {
-  return a % 2 === 0;
+// function checkEven(a) {
+//   return a % 2 === 0;
+// }
+// // const c = add(a, b);
+// // console.log(prompt("the sum of" + a + "+" + b + "=" + c));
+// const c = checkEven(a);
+// console.log(alert(c));
+
+// convert fahrenheit to celsius
+const fahrenheit = Number(prompt("enter the temperature in fahrenheit"));
+function tocelius(fahrenheit) {
+  return (5 / 9) * (fahrenheit - 32);
 }
-// const c = add(a, b);
-// console.log(prompt("the sum of" + a + "+" + b + "=" + c));
-const c = checkEven(a);
-console.log(alert(c));
+const value = tocelius(fahrenheit);
+console.log(alert(value));
+// // Nameless Function
+// default function
+// paramaterized function
+// inline function
+// closure function
+// implicit function
+// explicit function
