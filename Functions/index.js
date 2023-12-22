e006c36c0d1630c895fa89bffaf38bdf8befeeb9
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-22 17:27:03 +0545

    checking even through function

--- Functions/index.js
+++ Functions/index.js
@@ -5,5 +5,11 @@ const b = Number(prompt("enter any number"));
 function add(a, b) {
   return a + b;
 }
-const c = add(a, b);
-console.log(prompt("the sum of" + a + "+" + b + "=" + c));
+
+function checkEven(a) {
+  return a % 2 === 0;
+}
+// const c = add(a, b);
+// console.log(prompt("the sum of" + a + "+" + b + "=" + c));
+const c = checkEven(a);
+console.log(alert(c));
