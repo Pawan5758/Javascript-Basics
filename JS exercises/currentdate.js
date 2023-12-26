1a48d3d2b0764536cbbb54431e1bcbfd7021f9d0
Pawan Shrestha <pawanshrestha5758@gmail.com>
2023-12-27 00:31:03 +0545

    current date

---
+++ JS exercises/currentdate.js
@@ -0,0 +1,17 @@
+// get the current date
+var today = new Date();
+// get the day of the month
+var dd = today.getDate();
+// // get the day month
+// var mm = today.getMonth() + 1;
+// // get the year
+// var yyyy = today.getFullYear();
+// if (dd < 10) {
+//   dd = `0${dd}`;
+// }
+// if (mm < 10) {
+//   mm = `0${mm}`;
+// }
+// today = `${mm}-${dd}-${yyyy}`;
+// console.log(today);
+console.log(dd);
