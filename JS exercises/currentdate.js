7ab84a6f62f68733edb9cf100ff89b5ed0e78ad2
Pawan Shrestha <pawanshrestha5758@gmail.com>
2024-01-01 16:32:08 +0545

    changes

--- JS exercises/currentdate.js
+++ JS exercises/currentdate.js
@@ -2,16 +2,15 @@
 var today = new Date();
 // get the day of the month
 var dd = today.getDate();
-// // get the day month
-// var mm = today.getMonth() + 1;
-// // get the year
-// var yyyy = today.getFullYear();
-// if (dd < 10) {
-//   dd = `0${dd}`;
-// }
-// if (mm < 10) {
-//   mm = `0${mm}`;
-// }
-// today = `${mm}-${dd}-${yyyy}`;
-// console.log(today);
-console.log(dd);
+// get the day month
+var mm = today.getMonth() + 1;
+// get the year
+var yyyy = today.getFullYear();
+if (dd < 10) {
+  dd = `0${dd}`;
+}
+if (mm < 10) {
+  mm = `0${mm}`;
+}
+today = `${mm}-${dd}-${yyyy}`;
+console.log(today);
